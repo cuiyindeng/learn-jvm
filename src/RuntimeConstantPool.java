8eca533f31f20d677f52a3a9d22f1573c0ad97e0
cuiyindeng <543770144@qq.com>
2022-05-02 11:14:46 +0800

    update

--- src/RuntimeConstantPool.java
+++ src/RuntimeConstantPool.java
@@ -1,9 +1,11 @@
 public class RuntimeConstantPool {
     public static void main(String[] args) {
         String str1 = new StringBuilder("计算机").append("软件").toString();
+//        String str2 = "计算机软件";
+//        System.out.println(str1 == str2);
         System.out.println(str1.intern() == str1);
 
-        String str2 = new StringBuilder("ja").append("va").toString();
-        System.out.println(str2.intern() == str2);
+        String str3 = new StringBuilder("ja").append("va").toString();
+        System.out.println(str3.intern() == str3);
     }
 }
